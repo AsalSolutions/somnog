dd78102fe778c8f99a5a9a364bdfe6202e14f506
Amin Abdisamad <amin.abdisamad@b4finance.com>
2020-04-18 06:25:51 +0300

    Created backend directory, created speakers api for getting and posting speakers

---
+++ backend/model/Speaker.py
@@ -0,0 +1,56 @@
+from db import db, mm
+# **Speaker Schema:**
+# `(id,firstname,lastname,photo,description, company,job_title,description,social_acount)`
+
+
+class Speaker(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(150), nullable=False)
+    lastName = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(80), unique=True, nullable=True)
+    description = db.Column(db.String(255), nullable=True)
+    companyName = db.Column(db.String(140), nullable=True)
+    jobTitle = db.Column(db.String(120), nullable=True)
+    photo = db.Column(db.String(100), nullable=True)
+    socialAccount = db.Column(db.String(200), nullable=True)
+
+    def __init__(self, firstName, lastName, email,
+                 phone, description, companyName, jobTitle, photo, socialAccount):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phone = phone
+        self.description = description
+        self.companyName = companyName
+        self.jobTitle = jobTitle
+        self.photo = photo
+        self.socialAccount = socialAccount
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+class SpeakerSchema(mm.Schema):
+    class Meta:
+        fields = ('_id', 'firstName', 'lastName', 'email', 'phone',
+                  'description', 'companyName', 'jobTitle', 'Photo', 'socialAccount')
+
+
+# Create DB
+db.create_all()
+# # Insert Spearker
+# speaker_1 = Speaker(firstName="Hassan", lastName="Abdisamad",
+#                     email="hassan@example.com", jobTitle="Machine Developer")
+# db.session.add(speaker_1)
+# db.session.commit()
+
+# speaker = Speaker.query.all()
---
+++ backend/resources/speaker.py
@@ -0,0 +1,56 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from model.Speaker import Speaker, SpeakerSchema
+from db import db
+
+#
+
+speaker_schema = SpeakerSchema()
+speaker_schemas = SpeakerSchema(many=True)
+
+
+class SpeakerApi(Resource):
+    # Get Single Speaker
+    def get(self, id):
+        speaker = Speaker.query.get(id)
+        if speaker:
+            return speaker_schema.jsonify(speaker)
+        return ({"message": "Speaker not found"}), 404
+
+    # Delete Single Speaker
+    def delete(self, id):
+        speaker = Speaker.query.get(id)
+        if speaker:
+            db.session.delete(speaker)
+            db.session.commit()
+            return ({"message": "Speaker deleted sucessfully"}), 200
+        return ({"message": "speaker not found"}), 404
+
+
+class AddSpeaker(Resource):
+    def post(self):
+
+        fistName = request.json['firstName']
+        lastName = request.json['lastName']
+        email = request.json['email']
+        phone = request.json['phone']
+        description = request.json['description']
+        companyName = request.json['companyName']
+        jobTitle = request.json['jobTitle']
+        photo = request.json['photo']
+        socialAccount = request.json['socialAccount']
+        print(fistName, lastName, email, phone, description,
+              companyName, jobTitle, photo, socialAccount)
+        # init object from Speaker class
+        newSpeaker = Speaker(fistName, lastName, email, phone,
+                             description, companyName, jobTitle, photo, socialAccount)
+        # Save to database
+        newSpeaker.save()
+        return speaker_schema.jsonify(newSpeaker)
+
+
+class GetAllSpeakers(Resource):
+    def get(self):
+        speakers = Speaker.query.all()
+        result = speaker_schemas.dump(speakers)
+        return jsonify(result)
