b65ff1bf455cfb033a15b4640fd9103673b63c44
Amin Abdisamad <amin.abdisamad@b4finance.com>
2020-04-20 07:31:24 +0300

    Cleaned React Fronend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { speakerList: {}, isLoading: true };
+  componentDidMount() {
+    fetch("/v1/speaker")
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({ speakerList: result, isLoading: false });
+      });
+  }
+  speakerList = () => {
+    const { speakerList, isLoading } = this.state;
+    if (isLoading) {
+      return;
+    }
+    return speakerList.map((speaker, index) => {
+      return (
+        <div>
+          <ul>
+            <li key={speaker._id}>FirstName: {speaker.firstName}</li>
+            <li>LastName: {speaker.lastName}</li>
+            <li>Email: {speaker.email}</li>
+            <li>Company: {speaker.companyName}</li>
+            <li>Job Title: {speaker.jobTitle}</li>
+          </ul>
+        </div>
+      );
+    });
+  };
+  render() {
+    return (
+      <div>
+        {/* <SpeakerList speaker={this.state.speakerList} /> */}
+
+        <p>Speakers List</p>
+        <div>{this.speakerList()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
