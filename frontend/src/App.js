2332147bd4b9b3f219c6a8a2b558ec642baf6479
Amin Abdisamad <amin.abdisamad@b4finance.com>
2020-08-03 01:30:02 +0300

    Added layout - Dashboard Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router } from "react-router-dom";
 import history from "./history";
-import SpeakerList from "./components/speaker/SpeakerList";
-import CreateSpeaker from "./components/speaker/CreateSpeaker";
 
-import Home from "./components/pages/Home";
-import Header from "./components/layout/header";
+
+
+// import Header from "./components/layout/header";
+import Dashboard from './layout'
 
 function App() {
   return (
     <Router history={history}>
-      <Header />
-      <div>
+      <Dashboard />
+      {/* <div>
         <Route path="/" exact component={Home} />
         <Route path="/speaker" exact component={SpeakerList} />
         <Route path="/speaker/create" exact component={CreateSpeaker} />
-      </div>
+      </div> */}
     </Router>
   );
 }
---
+++ frontend/src/layout/index.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import SpeakerList from "../components/speaker/SpeakerList";
+import CreateSpeaker from "../components/speaker/CreateSpeaker";
+import Home from "../components/pages/Home";
+
+const { Header, Sider, Content } = Layout;
+
+class Dashboard extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+                <Link to="/"> Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                <Link to="/speaker"> Speakers</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+                <Link to="/speaker/create"> Add Speaker</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ paddingLeft: 20 }}>
+            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: this.toggle,
+            })}
+            @AminAbdisamad
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 610,
+            }}
+          >
+        {/* Content Should be displayed */}
+            <Route path="/" exact component={Home} />
+            <Route path="/speaker" exact component={SpeakerList} />
+            <Route path="/speaker/create" exact component={CreateSpeaker} />
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+// ReactDOM.render(<SiderDemo />, mountNode);
+export default Dashboard;
+
+
+
+
